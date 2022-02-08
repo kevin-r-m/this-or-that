071dc3a485e77f05ed5a2abbdef5ce06968007b7
kevin-r-m <kmclaughlinbtb@gmail.com>
2022-02-07 22:43:21 -0600

    inital commit

---
+++ src/components/Competition.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function Competition() {
+    return (
+        <div>
+            <div className="competition-wrapper">
+                <div className="competitor-container">
+                    
+                </div>
+                <div>
+                    <h3 class="competition-text">vs.</h3>
+                </div>
+                <div className="competitor-container">
+
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Competition;
---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function nav() {
+    return (
+        <div>
+            <nav class="nav-bar">
+                <div class="nav-list">
+                    <div class="nav-item">
+                        Competition
+                    </div>
+                    <div class="nav-item">
+                        Competitors
+                    </div>
+                    <div class="nav-item">
+                        Leaderboards
+                    </div>
+                    <div class="nav-item">
+                        My Account
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default nav;
