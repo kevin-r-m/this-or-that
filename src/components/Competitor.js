ea8ae4c2e383d4f27aa6a61a50a371a4e07b4dd6
kevin-r-m <kmclaughlinbtb@gmail.com>
2022-05-21 20:02:46 -0500

    styling updates

--- src/components/Competitor.js
+++ src/components/Competitor.js
@@ -24,24 +24,26 @@ function Competitor(props) {
     }
 
     return (
-        <div className="competitor-container">
-            <div className="competitor-title">
-                <h3>{props.competitorInfo.name}</h3>
-                <p className="more-info-toggle" onClick={handleClick} ref={elementRef}>See more info +</p>
-            </div>
-            <div className={"more-info-container" + showInfo}>
-                <p className="more-info-body">
-                    {props.competitorInfo.description}
-                    <a href="#">Go to competitor profile</a>
-                </p>
-            </div>
-            <div className="competitor-image-wrapper">
-                    <img src={props.competitorInfo.image} alt="batman" />
-                <div className="voting-container">
-                    <button className="vote-button" onClick={openModal} data-competitor={props.competitorInfo.name}>Cast vote for {props.competitorInfo.name}</button>
+        <>
+            <div className="competitor-container">
+                <div className="competitor-title">
+                    <h3>{props.competitorInfo.name}</h3>
+                    <p className="more-info-toggle" onClick={handleClick} ref={elementRef}>See more info +</p>
+                </div>
+                <div className={"more-info-container" + showInfo}>
+                    <div className="more-info-body">
+                        <p>{props.competitorInfo.description}</p>
+                        <a href="#" className="competitor-link" >Go to competitor profile</a>
+                    </div>
+                </div>
+                <div className="competitor-image-wrapper">
+                        <img src={props.competitorInfo.image} alt="batman" />
+                    <div className="voting-container">
+                        <button className="vote-button" onClick={openModal} data-competitor={props.competitorInfo.name}>Cast vote for {props.competitorInfo.name}</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
