f4e5d6dc06d58cb37a60cb0a06d35cbaad13fbbe
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-02-11 12:32:56 -0600

    nav component

--- app/(components)/Header/Nav/index.jsx
+++ app/(components)/Header/Nav/index.jsx
@@ -2,27 +2,28 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "./Nav.module.scss";
 
 function Nav() {
   const pathname = usePathname();
 
   return (
-    <nav>
+    <nav className={"container " + styles.nav}>
       <Link
         href="/competition"
-        className={pathname === "/competition" ? "active" : ""}
+        className={pathname === "/competition" ? styles.active : ""}
       >
         Today
       </Link>
       <Link
         href="/competitors"
-        className={pathname === "/competitors" ? "active" : ""}
+        className={pathname === "/competitors" ? styles.active : ""}
       >
         Competitors
       </Link>
       <Link
         href="/leaderboards"
-        className={pathname === "/leaderboards" ? "active" : ""}
+        className={pathname === "/leaderboards" ? styles.active : ""}
       >
         Leaderboards
       </Link>
