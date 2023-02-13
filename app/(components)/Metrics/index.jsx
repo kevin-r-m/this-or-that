4be67fa32c58139b9e37fe94b7db23dbfb5c6d23
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-02-12 23:12:43 -0600

    added new components and styling

--- app/(components)/Metrics/index.jsx
+++ app/(components)/Metrics/index.jsx
@@ -1,12 +1,20 @@
 import { MetricsContextProvider } from "./MetricsContextProvider";
 import Headline from "./Headline";
 import styles from "./Metrics.module.scss";
+import Leader from "./Leader";
+import TotalVotes from "./TotalVotes";
+import VoteTime from "./VoteTime";
 
 function Metrics() {
   return (
     <section className={styles.wrapper}>
       <MetricsContextProvider>
         <Headline />
+        <div className={styles.metricRow}>
+          <Leader />
+          <TotalVotes />
+          <VoteTime />
+        </div>
       </MetricsContextProvider>
     </section>
   );
