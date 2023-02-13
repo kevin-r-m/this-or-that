27f5ac10ff5afd774334ea3afc763eac12f8f16d
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-02-12 22:17:22 -0600

    updated nav component ui logic

--- app/(components)/Header/Nav/index.jsx
+++ app/(components)/Header/Nav/index.jsx
@@ -1,32 +1,48 @@
 "use client";
 
 import Link from "next/link";
+import { useRef } from "react";
 import { usePathname } from "next/navigation";
 import styles from "./Nav.module.scss";
+import AnimatedButton from "./AnimatedButton";
 
 function Nav() {
   const pathname = usePathname();
+  const elementRef = useRef();
+
+  function toggleMenu() {
+    const elementClasses = elementRef.current.classList;
+    elementClasses.toggle(styles.expanded);
+    if (elementClasses.contains(styles.expanded)) {
+      document.querySelector("body").style.overflow = "hidden";
+      return;
+    }
+    document.querySelector("body").style.overflow = "auto";
+  }
 
   return (
     <nav className={"container " + styles.nav}>
-      <Link
-        href="/competition"
-        className={pathname === "/competition" ? styles.active : ""}
-      >
-        Today
-      </Link>
-      <Link
-        href="/competitors"
-        className={pathname === "/competitors" ? styles.active : ""}
-      >
-        Competitors
-      </Link>
-      <Link
-        href="/leaderboards"
-        className={pathname === "/leaderboards" ? styles.active : ""}
-      >
-        Leaderboards
-      </Link>
+      <AnimatedButton callback={toggleMenu} />
+      <div ref={elementRef} className={styles.navItems}>
+        <Link
+          href="/competition"
+          className={pathname === "/competition" ? styles.active : ""}
+        >
+          Today
+        </Link>
+        <Link
+          href="/competitors"
+          className={pathname === "/competitors" ? styles.active : ""}
+        >
+          Competitors
+        </Link>
+        <Link
+          href="/leaderboards"
+          className={pathname === "/leaderboards" ? styles.active : ""}
+        >
+          Leaderboards
+        </Link>
+      </div>
     </nav>
   );
 }
