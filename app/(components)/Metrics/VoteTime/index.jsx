4f5979f000ae303437585fed5f5ed8feb324d21a
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-02-13 20:30:48 -0600

    added dynamic countdown to midnight

--- app/(components)/Metrics/VoteTime/index.jsx
+++ app/(components)/Metrics/VoteTime/index.jsx
@@ -1,14 +1,30 @@
 "use client";
 
-import { useMetricsContext } from "../MetricsContextProvider";
 import styles from "./voteTime.module.scss";
+import Countdown from "react-countdown";
+import { useRef } from "react";
 
 function VoteTime() {
+  const timeRef = useRef(millisecondsUntilMidnight());
+
+  function millisecondsUntilMidnight() {
+    const now = new Date();
+    const tomorrow = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate() + 1
+    );
+    const millisecondsUntilMidnight = tomorrow - now;
+    return millisecondsUntilMidnight;
+  }
+
   return (
     <div className={styles.timeContainer}>
       <p>Time left for voting:</p>
       <p>
-        <b>04:04:36</b>
+        <b>
+          <Countdown date={Date.now() + timeRef.current} />
+        </b>
       </p>
     </div>
   );
