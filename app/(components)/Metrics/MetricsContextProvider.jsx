f842bd5e26b26b1c69ed4b7c9784ad453a5896df
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-02-14 20:43:58 -0600

    new functions and refs to metric context

--- app/(components)/Metrics/MetricsContextProvider.jsx
+++ app/(components)/Metrics/MetricsContextProvider.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useRef } from "react";
 import data from "./testData.json";
 
 const MetricsContext = createContext();
@@ -11,10 +11,17 @@ export function useMetricsContext() {
 
 export function MetricsContextProvider({ children }) {
   const [competitionState] = useState(data);
+  const expandedRef = useRef(false);
+  const metricsRef = useRef();
+  const graphRef = useRef();
+
   const contextValues = {
     competitionState,
     calculateVoteDifference,
     millisecondsUntilMidnight,
+    handleMetricToggle,
+    graphRef,
+    metricsRef,
   };
 
   function calculateVoteDifference() {
@@ -33,6 +40,32 @@ export function MetricsContextProvider({ children }) {
     );
     const millisecondsUntilMidnight = tomorrow - now;
     return millisecondsUntilMidnight;
+  }
+
+  function handleMetricToggle() {
+    const graphHeight = graphRef.current.offsetHeight;
+    const metricsHeight = metricsRef.current.offsetHeight;
+    const metricStaticHeight = 159;
+    const paddingBuffer = 10;
+    const animation = {
+      duration: 250,
+      iterations: 1,
+      fill: "forwards",
+    };
+    let animationSettings;
+
+    if (!expandedRef.current) {
+      animationSettings = {
+        height: `${metricsHeight + (graphHeight + paddingBuffer)}px`,
+      };
+      expandedRef.current = true;
+    } else {
+      animationSettings = {
+        height: `${metricStaticHeight}px`,
+      };
+      expandedRef.current = false;
+    }
+    metricsRef.current.animate(animationSettings, animation);
   }
 
   return (
