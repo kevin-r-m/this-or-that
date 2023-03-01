1c85424770a6aed4662e5ae02c4995b45aae1f70
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-03-01 13:39:27 -0600

    updated callback prop value

--- app/(components)/Competition/CompetitorCard/VotingBody/index.jsx
+++ app/(components)/Competition/CompetitorCard/VotingBody/index.jsx
@@ -6,14 +6,14 @@ import styles from "./votingBody.module.scss";
 import { useCompetitionContext } from "../../CompetitionContextProvider";
 
 function VotingBody({ competitorName }) {
-  const { handleVoting } = useCompetitionContext();
+  const { handleVoting, handleVotingReset } = useCompetitionContext();
 
   return (
     <div className={styles.votingBody}>
       <p>Alright, confirm you vote for {competitorName}?</p>
       <div className={styles.buttonsContainer}>
         <Button callback={handleVoting} value="Confirm" confirm />
-        <Button callback={handleVoting} value="Take me back" decline />
+        <Button callback={handleVotingReset} value="Take me back" decline />
       </div>
     </div>
   );
