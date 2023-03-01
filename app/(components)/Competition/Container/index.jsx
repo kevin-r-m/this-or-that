43db1d92180246172f497ab1c7b19fcceed3138c
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-03-01 13:32:45 -0600

    updated props

--- app/(components)/Competition/Container/index.jsx
+++ app/(components)/Competition/Container/index.jsx
@@ -5,18 +5,19 @@ import CompetitorCard from "../CompetitorCard";
 import styles from "./container.module.scss";
 
 function Container() {
-  const { competitionState } = useCompetitionContext();
+  const { competitionState, votingForOne, votingForTwo } =
+    useCompetitionContext();
 
   return (
     <section className={styles.competitorCard}>
       <CompetitorCard
         competitorName={competitionState.competitorOne.name}
-        competitorState={competitionState.competitorOne.state.value}
+        competitorState={votingForOne}
         key={competitionState.competitorOne.name}
       />
       <CompetitorCard
         competitorName={competitionState.competitorTwo.name}
-        competitorState={competitionState.competitorTwo.state.value}
+        competitorState={votingForTwo}
         key={competitionState.competitorTwo.name}
       />
     </section>
