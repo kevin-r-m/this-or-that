f6326f890b9fad1b1e9a3e7fb48b2aa1b923178b
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-03-06 18:00:15 -0600

    transition for container

--- app/(components)/Competition/Container/index.jsx
+++ app/(components)/Competition/Container/index.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CompetitorTransition from "@/app/(hooks)/CompetitorTransition";
 import { useCompetitionContext } from "../CompetitionContextProvider";
 import CompetitorCard from "../CompetitorCard";
 import VotedCard from "../VotedCard";
@@ -9,12 +10,23 @@ function Container() {
   const { competitionState, votingForOne, votingForTwo, votingState } =
     useCompetitionContext();
 
-  return (
-    <section className={styles.competitorCards}>
-      {votingState.votedToday ? (
-        <VotedCard />
-      ) : (
-        <>
+  if (votingState.votedFor === null) {
+    return <></>;
+  } else {
+    return (
+      <section className={styles.competitorCards}>
+        <CompetitorTransition
+          duration={500}
+          state={!votingState.votedToday}
+          className={"body-one"}
+        >
+          <VotedCard />
+        </CompetitorTransition>
+        <CompetitorTransition
+          duration={500}
+          state={votingState.votedToday}
+          className={"body-two"}
+        >
           <CompetitorCard
             competitorName={competitionState.competitorOne.name}
             competitorState={votingForOne}
@@ -25,10 +37,10 @@ function Container() {
             competitorState={votingForTwo}
             key={competitionState.competitorTwo.name}
           />
-        </>
-      )}
-    </section>
-  );
+        </CompetitorTransition>
+      </section>
+    );
+  }
 }
 
 export default Container;
