5b0b4e1cdd6418537874bfef1c71c764fb3a2b13
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-03-06 18:00:37 -0600

    functions to wait for client rendering

--- app/(functions)/index.js
+++ app/(functions)/index.js
@@ -1,35 +1,41 @@
 // Function to set cookies
 export function setCookie(cookieName, value, expireDays, votedFor) {
-  const expireDate = new Date();
-  expireDate.setDate(expireDate.getDate() + expireDays);
-  document.cookie = `${cookieName}=["${votedFor}", "${getTodaysDate()}"]; expires=${expireDate.toUTCString()}; Secure`;
+  if (typeof window !== "undefined") {
+    const expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() + expireDays);
+    document.cookie = `${cookieName}=["${votedFor}", "${getTodaysDate()}"]; expires=${expireDate.toUTCString()}; Secure`;
+  }
 }
 
 // Returns the matched cookie & value based on the passed arg
 export function getCookie(cookieName) {
-  const match = document.cookie.match(
-    new RegExp("(^| )" + cookieName + "=([^;]+)")
-  );
-  if (match) {
-    const votedDate = JSON.parse(match[2])[1];
-    // returns boolean value of whether the date matches today
-    return checkCookieDate(votedDate);
+  if (typeof window !== "undefined") {
+    const match = document.cookie.match(
+      new RegExp("(^| )" + cookieName + "=([^;]+)")
+    );
+    if (match) {
+      const votedDate = JSON.parse(match[2])[1];
+      // returns boolean value of whether the date matches today
+      return checkCookieDate(votedDate);
+    }
   }
 }
 
 // Returns the matched cookie & value based on the passed arg
 export function getCookie_withValue(cookieName, value) {
-  const match = document.cookie.match(
-    new RegExp("(^| )" + cookieName + "=([^;]+)")
-  );
-  if (match) {
-    let returnData;
-    if (value === "votedFor") {
-      returnData = JSON.parse(match[2])[0].toString();
+  if (typeof window !== "undefined") {
+    const match = document.cookie.match(
+      new RegExp("(^| )" + cookieName + "=([^;]+)")
+    );
+    if (match) {
+      let returnData;
+      if (value === "votedFor") {
+        returnData = JSON.parse(match[2])[0].toString();
+      }
+      return returnData;
     }
-    return returnData;
+    return false;
   }
-  return false;
 }
 
 // Returns today's date as a string
