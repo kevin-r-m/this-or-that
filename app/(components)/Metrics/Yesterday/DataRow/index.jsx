4d8b9df2684bc086d02790e37330ecea5c760054
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-03-11 11:42:42 -0600

    yesterday component

---
+++ app/(components)/Metrics/Yesterday/DataRow/index.jsx
@@ -0,0 +1,22 @@
+import styles from "./datarow.module.scss";
+import classNames from "classnames/bind";
+
+function DataRow({ content, winner, loser, totalVotes, votes, ...props }) {
+  const cx = classNames.bind(styles);
+
+  const containerClasses = cx({
+    container: true,
+    totalVotes: totalVotes,
+    votes: votes,
+    winner: winner,
+    loser: loser,
+  });
+
+  return (
+    <div className={containerClasses} {...props}>
+      <p>{content}</p>
+    </div>
+  );
+}
+
+export default DataRow;
