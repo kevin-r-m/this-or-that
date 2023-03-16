70d7473a450a21b715539055764d2684ec5ca544
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-03-16 15:45:55 -0500

    useEffect

--- app/(components)/CompetitorBody/index.jsx
+++ app/(components)/CompetitorBody/index.jsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import data from "@/app/(data)/competitors.json";
 import Image from "next/image";
 import placeholder from "@/public/images/placeholder-image.webp";
 import styles from "./competitor-body.module.scss";
 
 function CompetitorBody({ id }) {
+  const [competitorData, setCompetitorData] = useState({});
   function handleCompetitorQuery(id) {
     const competitor = data.filter(
       (item) => item.id.toString() === id.toString()
@@ -14,7 +15,9 @@ function CompetitorBody({ id }) {
     return competitor[0];
   }
 
-  const competitor = handleCompetitorQuery(id);
+  useEffect(() => {
+    setCompetitorData(handleCompetitorQuery(id));
+  }, [id]);
 
   const imgStyles = {
     width: "100%",
@@ -23,12 +26,12 @@ function CompetitorBody({ id }) {
 
   return (
     <>
-      <h1>Batman</h1>
+      <h1>{competitorData.name}</h1>
       <section>
         <div className={styles.image}>
           <Image
             src={placeholder}
-            alt={`Image of Batman`}
+            alt={`Image of ${competitorData.name}`}
             sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               33vw"
@@ -36,7 +39,7 @@ function CompetitorBody({ id }) {
             priority
           />
         </div>
-        <p>Random facts about Batman</p>
+        <p>Random facts about {competitorData.name}</p>
       </section>
     </>
   );
