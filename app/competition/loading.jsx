f8efc760504c824ddaa225220dd34c570abacf67
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-03-19 20:20:16 -0500

    updating loading functionality

--- app/competition/loading.jsx
+++ app/competition/loading.jsx
@@ -1,3 +1,5 @@
+import Skeleton from "../(components)/Competition/Skeleton";
+
 export default function Loading() {
-  return <p>Loading...</p>;
+  return <Skeleton />;
 }
--- app/competition/page.jsx
+++ app/competition/page.jsx
@@ -1,7 +1,13 @@
 import Metrics from "../(components)/Metrics";
 import Competition from "../(components)/Competition";
 
-function page() {
+async function page() {
+  await delay(1000);
+
+  function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
   return (
     <>
       <Metrics />
