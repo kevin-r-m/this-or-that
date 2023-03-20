4a1febf3e56ffc5c09001f81679268cdef565b61
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-03-19 20:57:50 -0500

    search component updates

--- app/(components)/CompetitorsList/Search/index.jsx
+++ app/(components)/CompetitorsList/Search/index.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import styles from "./search.module.scss";
+import { useCompetitorListContext } from "../CompetitorListContextProvider";
 
 function Search() {
+  const { formRef, handleCompetitorSearch, handleFormReset } =
+    useCompetitorListContext();
+
   return (
-    <form className={styles.form} onSubmit={() => console.log("boom")}>
-      <input type="text" placeholder="Search by name..." />
-      <input type="submit" value="Go" />
-    </form>
+    <>
+      <form
+        ref={formRef}
+        className={styles.form}
+        onSubmit={handleCompetitorSearch}
+      >
+        <input type="text" name="name" placeholder="Search by name..." />
+        <input type="submit" value="Go" />
+      </form>
+      {formRef.current && formRef.current.name.value ? (
+        <button className={styles.formReset} onClick={handleFormReset}>
+          Reset search
+        </button>
+      ) : null}
+    </>
   );
 }
 
