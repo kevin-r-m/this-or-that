bc4df891d8918899f885379e3bfa9e27b0777894
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-11-24 14:51:13 -0700

    @next/font codemod

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./(styles)/base.scss";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import Header from "./(components)/Header";
 import Footer from "./(components)/Footer";
 
