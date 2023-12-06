3b949bffe7986c5911f2f43a97facacb049aa8d0
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-12-05 21:05:01 -0600

    updated expand button

--- app/(components)/Metrics/ExpandButton/index.jsx
+++ app/(components)/Metrics/ExpandButton/index.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./expandButton.module.scss";
 
 function ExpandButton({ callback }) {
+  const [active, setActive] = useState(false);
+
+  function handleClick(e) {
+    setActive((prevState) => !prevState);
+
+    if (callback) callback(e);
+  }
+
   return (
-    <button className={styles.expandButton} onClick={callback}>
-      <Image src="/images/cross.svg" alt="Toggle" width={"20"} height={"20"} />
+    <button className={styles.expandButton} onClick={handleClick}>
+      <Image className={active && styles.active} src="/images/caret.svg" alt="Toggle" width={"20"} height={"20"} />
     </button>
   );
 }
