73597c01e4e4dd37710a03f485e3e60d29a88af0
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-12-24 00:59:14 -0600

    expand button logic

--- app/(components)/Metrics/ExpandButton/index.jsx
+++ app/(components)/Metrics/ExpandButton/index.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./expandButton.module.scss";
+import classnames from "classnames/bind";
 
 function ExpandButton({ onClick, isActive }) {
+  const [isAnimated, setIsAnimated] = useState(false);
+
+  function handleClick(e) {
+    setIsAnimated(true);
+    onClick(e);
+  }
+
+  const cx = classnames.bind(styles);
+
+  const buttonClasses = cx({
+    expandButton: true,
+    animated: isAnimated,
+    active: isActive,
+  });
 
   return (
-    <button className={styles.expandButton} onClick={onClick}>
-      <Image className={isActive ? styles.active : undefined} src="/images/caret.svg" alt="Toggle" width={"20"} height={"20"} />
-    </button>
+    <div className={styles.shadow}>
+      <button className={buttonClasses} onClick={handleClick} title="Toggle Metric Graph">
+        <Image src="/images/caret.svg" alt="Toggle" width={"20"} height={"20"} />
+      </button>
+    </div>
   );
 }
 
