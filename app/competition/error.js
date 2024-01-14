aef361b38b00a3396edfc36d0a8ea8c4775d6564
kevin-r-m <kmclaughlinbtb@gmail.com>
2024-01-14 12:08:10 -0600

    placeholder error handling

---
+++ app/competition/error.js
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+function Error(props) {
+    return (
+        <section className='container'>
+            <p>Uh oh, something went wrong. Please try again or check back later.</p>
+        </section>
+    );
+}
+
+export default Error;
