a5ff1d67b7b47d31c8832e45fb8742257bb8bcdf
kevin-r-m <kmclaughlinbtb@gmail.com>
2024-01-14 12:12:00 -0600

    services

---
+++ app/services/api/competitor.js
@@ -0,0 +1,29 @@
+async function handleAPIRequest(resource, options) {
+    const res = await fetch(resource, options);
+
+    if(!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    return res.json()
+
+}
+
+async function getCompetition() {
+    "use server";
+
+    const resource = `${process.env.API_URL}/api/competition`;
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": process.env.API_KEY
+        },
+    }
+
+    const competitionData = await handleAPIRequest(resource, options);
+
+    return competitionData;
+}
+
+export { getCompetition }
