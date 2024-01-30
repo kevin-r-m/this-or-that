6669215f477352b802613d133215fc476fdc8b4e
kevin-r-m <kmclaughlinbtb@gmail.com>
2024-01-29 20:37:56 -0600

    Remove unused prop from Error component

--- app/competition/error.js
+++ app/competition/error.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-function Error(props) {
+function Error() {
     return (
         <section className='container'>
             <p>Uh oh, something went wrong. Please try again or check back later.</p>
