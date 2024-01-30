e503add206ae09f50a25be781550b64a401622c6
kevin-r-m <kmclaughlinbtb@gmail.com>
2024-01-29 20:51:27 -0600

    working on yesterday luck context

--- app/components/Yesterday/YesterdayContextProvider.jsx
+++ app/components/Yesterday/YesterdayContextProvider.jsx
@@ -2,6 +2,9 @@
 
 import { useContext, createContext, useState } from 'react';
 import data from '../../data/competition.json';
+import { getCookie } from '@/app/functions';
+
+const GOOD_LUCK_COOKIE = 'goodluck';
 
 const YesterdayContext = createContext();
 
@@ -11,9 +14,25 @@ export function useYesterdayContext() {
 
 export function YesterdayContextProvider({ children })  {
     const [competitionState] = useState(data);
+    const [goodLuckCount, setGoodLuckCount] = useState(274);
+    const [wished, setWished] = useState(false);
+
+    function wishGoodLuck() {
+        // hit api to update good luck count
+
+        // if successful update state with response
+        setGoodLuckCount(prevState => prevState + 1);
+
+        // update state to show that user has wished
+        setWished(true);
+        
+    }
 
     const contextValues = {
         competitionState,
+        goodLuckCount, 
+        wished,
+        wishGoodLuck
     }
 
     return (
