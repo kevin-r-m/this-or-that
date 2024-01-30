3f0133fe274734fc7e0bf7cfdac278b704d95a8f
kevin-r-m <kmclaughlinbtb@gmail.com>
2024-01-29 20:39:29 -0600

    Confetti throw

--- app/components/Yesterday/Goodluck/index.jsx
+++ app/components/Yesterday/Goodluck/index.jsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
 import { Button, Image } from "../../Atoms";
 import thumbsUp from "../../../../public/images/dislike.svg";
 import styles from "./goodluck.module.scss";
+import Confetti from "react-dom-confetti";
+import { useYesterdayContext } from "../YesterdayContextProvider";
 
-function GoodLuckButton({ onClick, value }) {
+function GoodLuckButton({ onClick, value, wished }) {
 
-    function handleClick(e) {
-        onClick(e);
-      }
+      const confettiConfig = {
+        angle: 90,
+        spread: 180,
+        startVelocity: 40,
+        elementCount: 70,
+        dragFriction: 0.12,
+        duration: 3000,
+        stagger: 3,
+        width: "10px",
+        height: "10px",
+        perspective: "500px",
+        rotate: true,
+      };
 
     return (
-        <Button onClick={handleClick}>
+        <Button onClick={onClick} disabled={wished}>
+            <Confetti active={wished} config={confettiConfig} />
             <span>{value}</span>
         </Button>
     );
@@ -18,20 +30,16 @@ function GoodLuckButton({ onClick, value }) {
 }
 
 function Goodluck({ loserName }) {
-    const [goodLuckCount, setGoodLuckCount] = useState(274);
-
-    function wishGoodLuck() {
-        setGoodLuckCount(prevState => prevState + 1);
-    }
+    const { goodLuckCount, wished, wishGoodLuck } = useYesterdayContext();
 
     return (
         <div className={styles.goodluck}>
             <p>Want to wish good luck to {loserName} in their next competition?</p>
             <div className={styles.wish}>
-                <GoodLuckButton value="Wish good luck!" onClick={wishGoodLuck} />
+                <GoodLuckButton value={wished ? `${loserName} says thanks!` : "Wish good luck!"} onClick={wishGoodLuck} wished={wished} />
                 <div className={styles.count}>
                     <span>{goodLuckCount}</span>
-                    <div className={styles.thumbContainer}>
+                    <div className={styles.thumbContainer} title={`${goodLuckCount} wishes of good luck`}>
                         <Image source={thumbsUp} alt="Thumbs Up" />
                     </div>
                 </div>
